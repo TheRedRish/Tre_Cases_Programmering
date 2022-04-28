3ca0977460290d9acd3380912d916d5fe34f454e
RuneHansen <rune9485@elev.tec.dk>
2022-04-28 10:21:46 +0200

    removed unneccesary line

--- MyClassLibrary/PasswordCheck.cs
+++ MyClassLibrary/PasswordCheck.cs
@@ -4,10 +4,6 @@ namespace MyClassLibrary
 {
     public class PasswordCheck
     {
-        private static int?[] MyMethod(int x, int y = 20)
-        {
-            return new int?[3] { 5 + x, y, null };
-        }
         //Tjekker om alle password kriterierne er opfyldt
         public bool DoesPasswordOverholdEveryCondition(string username, string password)
         {
